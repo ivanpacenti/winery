992dc50d30cd910151ed2ad10e5ecf2e5d7866c5
ivan <s1103326@studenti.univpm.it>
2025-01-09 10:12:51 +0100

    creata landing page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+  constructor(private router: Router) {}
 
+  enterShop() {
+    // Qui puoi navigare alla pagina dello shop (quando esisterà)
+    this.router.navigate(['/shop']);
+  }
 }
