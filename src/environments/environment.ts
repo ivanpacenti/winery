11365e83f5deee3c211b5a9c1505659000b6774e
ivan <s1103326@studenti.univpm.it>
2025-03-04 12:57:39 +0100

    corretto indirizzo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://16.170.247.142:5103/api' // URL del backend
+  apiUrl: 'https://api.lemarchewine.com/api' // URL del backend
 };
